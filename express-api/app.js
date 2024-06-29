876ffaca3667ca2b45ffb6f250c9ab6ec450d3d1
Polina Kostukevich <kostukevich.p9@gmail.com>
2024-06-29 18:48:52 +0300

    add db models

--- express-api/app.js
+++ express-api/app.js
@@ -3,24 +3,23 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const fs = require("fs");
 
 const app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "jade");
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api", require("./routes"));
+
+if (fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- express-api/routes/index.js
+++ express-api/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const uploadDestination = "uploads";
+
+const storage = diskStorage({
+  destination: uploadDestination,
+  filename: function (req, file, callback) {
+    callback(null, file.originalName);
+  },
+});
+
+const uploads = multer({ storage: storage });
+
+router.get("/register", (req, res) => {
+  res.send("register");
 });
 
 module.exports = router;
